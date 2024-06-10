b907a051a58375322cb71ecdb150c2b02d01ef01
Kevin Secaida <3001920290101@ingenieria.usac.edu.gt>
2024-06-10 11:47:03 -0600

    Actualización
    Backend a medias

--- Proyecto1/backend/main.go
+++ Proyecto1/backend/main.go
@@ -2,104 +2,17 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
 	"encoding/json"
 	"fmt"
-
-	//	"strconv"
-	//"io/ioutil"
 	"log"
-	//"math"
 	"net/http"
 	"os/exec"
 	"strconv"
 	"time"
 
-	//"strings"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-
-	//"github.com/rs/cors"
-	"os"
-	//"github.com/shirou/gopsutil/cpu"
 )
-
-var conexion = ConectarBD()
-
-func ConectarBD() *sql.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	// Conectar a MySQL sin especificar la base de datos
-	conexionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/?parseTime=true", dbUser, dbPassword, dbHost, dbPort)
-	conexion, err := sql.Open("mysql", conexionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Verificar si la base de datos existe, de lo contrario, crearla
-	if err := verificarBaseDatos(conexion, dbName); err != nil {
-		log.Fatal(err)
-	}
-	// Conectar a la base de datos especificada
-	conexionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
-	conexion, err = sql.Open("mysql", conexionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		crearTabla(conexion, "ram")
-		crearTabla(conexion, "cpu")
-		fmt.Println("Conexion con MySQL Correcta")
-	}
-	return conexion
-}
-
-func verificarBaseDatos(db *sql.DB, dbName string) error {
-	// Consultar si la base de datos existe
-	query := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName)
-	_, err := db.Exec(query)
-	return err
-}
-
-func crearTabla(db *sql.DB, tabla string) {
-	// Query para verificar si la tabla existe
-	query := fmt.Sprintf("SELECT 1 FROM %s LIMIT 1", tabla)
-	_, err := db.Exec(query)
-	if err != nil {
-		// La tabla no existe, la creamos
-		crearQuery := fmt.Sprintf("CREATE TABLE %s (FECHA VARCHAR(50) PRIMARY KEY, Porcentaje INT)", tabla)
-		_, err := db.Exec(crearQuery)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Tabla %s creada correctamente\n", tabla)
-	}
-}
-
-func crearDB(conexion *sql.DB) {
-	query := "CREATE DATABASE IF NOT EXISTS Proyecto1;"
-	_, err := conexion.Exec(query)
-	if err != nil {
-		crearQuery := "USE Proyecto1;"
-		_, err := conexion.Exec(crearQuery)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		fmt.Println("DATA BASE CREADA")
-	}
-
-}
 
 type Datacpu struct {
 	Fecha      string `json:"fecha"`
@@ -170,57 +83,11 @@ func StartProcess(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error al iniciar el proceso", http.StatusInternalServerError)
 		return
 	}
+
 	// Obtener el comando con PID
 	process = cmd
+
 	fmt.Fprintf(w, "Proceso iniciado con PID: %d y estado en espera", process.Process.Pid)
-}
-
-func StopProcess(w http.ResponseWriter, r *http.Request) {
-	pidStr := r.URL.Query().Get("pid")
-	if pidStr == "" {
-		http.Error(w, "Se requiere el parámetro 'pid'", http.StatusBadRequest)
-		return
-	}
-
-	pid, err := strconv.Atoi(pidStr)
-	if err != nil {
-		http.Error(w, "El parámetro 'pid' debe ser un número entero", http.StatusBadRequest)
-		return
-	}
-
-	// Enviar señal SIGSTOP al proceso con el PID proporcionado
-	cmd := exec.Command("kill", "-SIGSTOP", strconv.Itoa(pid))
-	err = cmd.Run()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error al detener el proceso con PID %d", pid), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Proceso con PID %d detenido", pid)
-}
-
-func ResumeProcess(w http.ResponseWriter, r *http.Request) {
-	pidStr := r.URL.Query().Get("pid")
-	if pidStr == "" {
-		http.Error(w, "Se requiere el parámetro 'pid'", http.StatusBadRequest)
-		return
-	}
-
-	pid, err := strconv.Atoi(pidStr)
-	if err != nil {
-		http.Error(w, "El parámetro 'pid' debe ser un número entero", http.StatusBadRequest)
-		return
-	}
-
-	// Enviar señal SIGCONT al proceso con el PID proporcionado
-	cmd := exec.Command("kill", "-SIGCONT", strconv.Itoa(pid))
-	err = cmd.Run()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error al reanudar el proceso con PID %d", pid), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Proceso con PID %d reanudado", pid)
 }
 
 func KillProcess(w http.ResponseWriter, r *http.Request) {
@@ -247,170 +114,12 @@ func KillProcess(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Proceso con PID %d ha terminado", pid)
 }
 
-func Insertdatacpu(info map[string]interface{}) {
-	jsonData, err := json.Marshal(info)
-
-	if err != nil {
-		fmt.Println("eram1")
-		log.Fatal(err)
-	}
-	fmt.Println("string(jsonData)")
-	fmt.Println(string(jsonData))
-
-	response := &http.Response{}
-	request, err := http.NewRequest("POST", "http://localhost:5200/api/registrocpu", bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("eram2")
-		log.Fatal(err)
-	}
-	request.Header.Set("Content-Type", "application/json")
-	response, err = http.DefaultClient.Do(request)
-	if err != nil {
-		fmt.Println("eram3")
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	var respuesta Respuestacpu
-	json.NewDecoder(response.Body).Decode(&respuesta)
-	fmt.Println(respuesta.Mensaje)
-	fmt.Println("respuesta.Mensaje")
-
-}
-func Insertdataram(info map[string]interface{}) {
-
-	jsonData, err := json.Marshal(info)
-
-	if err != nil {
-		fmt.Println("eram1")
-		log.Fatal(err)
-	}
-	response := &http.Response{}
-	request, err := http.NewRequest("POST", "http://localhost:5200/api/registroram", bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("eram2")
-		log.Fatal(err)
-	}
-	request.Header.Set("Content-Type", "application/json")
-	response, err = http.DefaultClient.Do(request)
-	if err != nil {
-		fmt.Println("eram3")
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	var respuesta Respuestaram
-	json.NewDecoder(response.Body).Decode(&respuesta)
-	fmt.Println(respuesta.Mensaje)
-	fmt.Println("respuesta.Mensaje")
-
-}
-func Registrocpu(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-
-	var dataCpu Datacpu
-	var respuesta Respuestacpu
-
-	json.NewDecoder((request.Body)).Decode(&dataCpu)
-	respuesta.Mensaje = "Registro Exitoso"
-
-	query := `INSERT INTO cpu (Fecha, Porcentaje) VALUES (?,?);`
-	result, err := conexion.Exec(query, dataCpu.Fecha, dataCpu.Porcentaje)
-	if err != nil {
-		fmt.Println(err)
-		respuesta.Mensaje = "Error al registrar"
-	}
-	fmt.Println(result)
-	fmt.Println(respuesta)
-	response.Header().Set("Content-Type", "application/json")
-	response.WriteHeader(http.StatusCreated)
-	json.NewEncoder(response).Encode(respuesta)
-}
-func Registroram(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	//var discoReg Disco
-	//var respuesta Respuesta
-	var ramReg Dataram
-	//now := time.Now()
-	//re := fmt.Sprint("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	var respuesta Respuestaram
-
-	json.NewDecoder((request.Body)).Decode(&ramReg)
-	respuesta.Mensaje = "Registro Exitoso"
-
-	query := `INSERT INTO ram (Fecha, Porcentaje) VALUES (?,?);`
-	result, err := conexion.Exec(query, ramReg.Fecha, ramReg.Porcentaje)
-	if err != nil {
-		fmt.Println(err)
-		respuesta.Mensaje = "Error al registrar"
-	}
-	fmt.Println(result)
-	fmt.Println(respuesta)
-	response.Header().Set("Content-Type", "application/json")
-	response.WriteHeader(http.StatusCreated)
-	json.NewEncoder(response).Encode(respuesta)
-}
-
-func Getdatcpu(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	var lista []Datacpu
-	//now := time.Now()
-	//re := fmt.Sprint("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	query := "select * from cpu;"
-	result, err := conexion.Query(query)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for result.Next() {
-		var datcpu Datacpu
-
-		err = result.Scan(&datcpu.Fecha, &datcpu.Porcentaje)
-		if err != nil {
-			fmt.Println(err)
-		}
-		lista = append(lista, datcpu)
-	}
-	json.NewEncoder(response).Encode(lista)
-}
-func Getdatram(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	var lista []Dataram
-	//now := time.Now()
-	//re := fmt.Sprint("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	query := "select * from ram;"
-	result, err := conexion.Query(query)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for result.Next() {
-		var datram Dataram
-
-		err = result.Scan(&datram.Fecha, &datram.Porcentaje)
-		if err != nil {
-			fmt.Println(err)
-		}
-		lista = append(lista, datram)
-	}
-	json.NewEncoder(response).Encode(lista)
-}
-
 func CPUModuleHandler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(" ==================== DATOS MODULO CPU ==================== ")
-	//fmt.Println(" ")
-
 	cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_1s2024")
 	outCpu, err := cmdCpu.CombinedOutput()
 	if err != nil {
 		fmt.Println("eerror", err)
-		//fmt.Println(outCpu)
 	}
-	//	output := string(outCpu[:])
-	//	fmt.Println(output)
-
-	//---CPU
-	//fmt.Println("-------------------- CPU --------------------")
 	var cpu_info Cpu
 	err = json.Unmarshal([]byte(outCpu), &cpu_info)
 	if err != nil {
@@ -421,56 +130,34 @@ func CPUModuleHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode((cpu_info))
 }
-func RAMModuleHandler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(" ==================== DATOS MODULO RAM ==================== ")
-	//fmt.Println(" ")
 
+func RAMModuleHandler(w http.ResponseWriter, r *http.Request) {
 	cmdRam := exec.Command("sh", "-c", "cat /proc/ram_so1_1s2024")
 	outRam, err := cmdRam.CombinedOutput()
 	if err != nil {
-		fmt.Println("eerror", err)
-		//fmt.Println(outCpu)
+		fmt.Println("error", err)
 	}
-	//	outputt := string(outRam[:])
-	//	fmt.Println(outputt)
-	//---RAM
-	//fmt.Println("-------------------- RAM --------------------")
 	var ram_info Ram
 	err = json.Unmarshal([]byte(outRam), &ram_info)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println("ram_info")
-	//fmt.Println(ram_info)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode((ram_info))
 
 }
+
 func main() {
 	router := mux.NewRouter()
-	//	routersql := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		// No hacer nada aquí, solo devolver un código 200 o 404
 		w.WriteHeader(http.StatusNoContent)
 	}).Methods("GET", "POST")
 	router.HandleFunc("/api/cpu", CPUModuleHandler).Methods("GET", "POST")
 	router.HandleFunc("/api/ram", RAMModuleHandler).Methods("GET", "POST")
-	router.HandleFunc("/api/datram", Getdatram).Methods("GET")
-	router.HandleFunc("/api/registroram", Registroram).Methods("POST")
-	router.HandleFunc("/api/datcpu", Getdatcpu).Methods("GET")
-	router.HandleFunc("/api/registrocpu", Registrocpu).Methods("POST")
-
-	router.HandleFunc("/api/datasrames", GetDataram2).Methods("GET")
-
-	router.HandleFunc("/api/datascpus", GetDatacpu2).Methods("GET")
 
 	router.HandleFunc("/api/start", StartProcess)
-	router.HandleFunc("/api/stop", StopProcess)
-	router.HandleFunc("/api/resume", ResumeProcess)
 	router.HandleFunc("/api/kill", KillProcess)
-
-	//crearDB(conexion)
 
 	go func() {
 		log.Fatal(http.ListenAndServe(":5200", handlers.CORS()(router)))
@@ -483,60 +170,47 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			//	time.Sleep(1000)
 			now := time.Now()
 			second1 := now.Second()
 			re := strconv.Itoa(now.Year()) + "-" + fmt.Sprintf("%02d", now.Month()) + "-" + fmt.Sprintf("%02d", now.Day()) + "-" + fmt.Sprintf("%02d", now.Hour()) + "-" + fmt.Sprintf("%02d", now.Minute()) + "-" + fmt.Sprintf("%02d", second1)
 			fmt.Println("re")
 			fmt.Println(re)
-
-			fmt.Println(" ==================== DATOS MODULO CPU ==================== ")
-			fmt.Println(" ")
-
-			cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_1s2024")
+			cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_jun2024")
 			outCpu, err := cmdCpu.CombinedOutput()
 			if err != nil {
-				fmt.Println("eerror", err)
-				//fmt.Println(outCpu)
+				fmt.Println("error", err)
+				return
 			}
-			//	output := string(outCpu[:])
-			//	fmt.Println(output)
 
 			//---CPU
 			fmt.Println("-------------------- CPU --------------------")
-			var cpu_info Cpu
-			err = json.Unmarshal([]byte(outCpu), &cpu_info)
+			var cpuInfo Cpu
+			err = json.Unmarshal(outCpu, &cpuInfo)
 			if err != nil {
 				fmt.Println(err)
-			}
-			//Mandar respuesta
-			go sendToAPI("/cpu", cpu_info)
-			infocpu := map[string]interface{}{
-				"Fecha":      re,
-				"Porcentaje": cpu_info.Porcentaje,
+				return
 			}
 
-			Insertdatacpu(infocpu)
+			fmt.Println("porcentaje:", cpuInfo.Porcentaje)
+			fmt.Println("cpu informacion de processos: ", cpuInfo.Procesos)
 
-			//	time.Sleep(1000)
-			noww := time.Now()
-			second := noww.Second()
-			if now.Second() == noww.Second() {
-				second = second + 1
+			//Mandar respuesta (ejemplo simple de uso)
+			for _, proceso := range cpuInfo.Procesos {
+				// fmt.Printf("Process ID: %d, Name: %s, User: %d, State: %d, RAM: %d\n",
+				// 	proceso.Pid, proceso.Nombre, proceso.Usuario, proceso.Estado, proceso.Ram)
+				for _, child := range proceso.Chil {
+					fmt.Printf("  Child Process ID: %d, Name: %s, State: %d, Parent ID: %d\n",
+						child.Pid, child.Nombre, child.Estado, child.Padre)
+				}
 			}
-			ree := strconv.Itoa(now.Year()) + "-" + fmt.Sprintf("%02d", now.Month()) + "-" + fmt.Sprintf("%02d", now.Day()) + "-" + fmt.Sprintf("%02d", now.Hour()) + "-" + fmt.Sprintf("%02d", now.Minute()) + "-" + fmt.Sprintf("%02d", second)
-
+			go sendToAPI("/cpu", cpuInfo.Porcentaje)
 			fmt.Println(" ==================== DATOS MODULO RAM ==================== ")
 			fmt.Println(" ")
-
-			cmdRam := exec.Command("sh", "-c", "cat /proc/ram_so1_1s2024")
+			cmdRam := exec.Command("sh", "-c", "cat /proc/ram_so1_jun2024")
 			outRam, err := cmdRam.CombinedOutput()
 			if err != nil {
-				fmt.Println("eerror", err)
-				//fmt.Println(outCpu)
+				fmt.Println("error", err)
 			}
-			//	outputt := string(outRam[:])
-			//	fmt.Println(outputt)
 			//---RAM
 			fmt.Println("-------------------- RAM --------------------")
 			var ram_info Ram
@@ -544,17 +218,9 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			//fmt.Println("ram_info")
-			///fmt.Println(ram_info)
-			go sendToAPI("/ram", ram_info)
+			go sendToAPI("/ram", ram_info.Porcentaje)
 			fmt.Println(ram_info.Porcentaje)
 			fmt.Println(ram_info.En_uso)
-			inforam := map[string]interface{}{
-				"Fecha":      ree,
-				"Porcentaje": ram_info.Porcentaje,
-			}
-			Insertdataram(inforam)
-			//Mandar respuesta
 
 		}
 	}
@@ -583,51 +249,4 @@ func sendToAPI(route string, data interface{}) {
 	}
 
 	log.Printf("Datos enviados a la ruta %s\n", route)
-}
-
-func GetDataram2(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	var lista []Dataram
-	//now := time.Now()
-	//re := fmt.Sprint("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	query := "SELECT Fecha, Porcentaje FROM ram ORDER BY Fecha DESC LIMIT 10;"
-	result, err := conexion.Query(query)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for result.Next() {
-		var datram Dataram
-
-		err = result.Scan(&datram.Fecha, &datram.Porcentaje)
-		if err != nil {
-			fmt.Println(err)
-		}
-		lista = append(lista, datram)
-	}
-	json.NewEncoder(response).Encode(lista)
-}
-
-func GetDatacpu2(response http.ResponseWriter, request *http.Request) {
-
-	response.Header().Add("content-type", "application/json")
-	var lista []Datacpu
-	//now := time.Now()
-	//re := fmt.Sprint("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-	query := "SELECT Fecha, Porcentaje FROM cpu ORDER BY Fecha DESC LIMIT 10;"
-	result, err := conexion.Query(query)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for result.Next() {
-		var datcpu Datacpu
-
-		err = result.Scan(&datcpu.Fecha, &datcpu.Porcentaje)
-		if err != nil {
-			fmt.Println(err)
-		}
-		lista = append(lista, datcpu)
-	}
-	json.NewEncoder(response).Encode(lista)
 }
