6a720b339943a5f101a8813a1774e6aa4c46c320
Kevin Secaida <3001920290101@ingenieria.usac.edu.gt>
2024-06-12 01:00:28 -0600

    actualización
    dockerización y extracción de datos correcta.

--- Proyecto1/backend/go.mod
+++ Proyecto1/backend/go.mod
@@ -3,25 +3,27 @@ module main
 go 1.22.0
 
 require (
-	github.com/felixge/httpsnoop v1.0.3 // indirect
-	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gofiber/fiber v1.14.6
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver v1.15.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/gorilla/handlers v1.5.2 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
-	github.com/rs/cors v1.10.1 // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
-	github.com/tklauser/go-sysconf v0.3.13 // indirect
-	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	go.mongodb.org/mongo-driver v1.15.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
--- Proyecto1/backend/main.go
+++ Proyecto1/backend/main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
+	"strings"
+
+	"main/Database"
 	"os/exec"
 	"strconv"
 	"time"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/gofiber/fiber"
 )
-
-type Datacpu struct {
-	Fecha      string `json:"fecha"`
-	Porcentaje int    `json:"porcentaje"`
-}
-
-type Dataram struct {
-	Fecha      string `json:"fecha"`
-	Porcentaje int    `json:"porcentaje"`
-}
 
 type child struct {
 	Pid    int    `json:"pid"`
@@ -36,14 +23,13 @@ type child struct {
 }
 
 type Process struct {
-	Pid     int     `json:"pid"`
-	Nombre  string  `json:"name"`
-	Usuario int     `json:"usuario"`
-	Estado  int     `json:"state"`
-	Ram     int     `json:"ram"`
-	Padre   int     `json:"pidPadre"`
-	Chil    []child `json:"child"`
+	Pid    int     `json:"pid"`
+	Nombre string  `json:"name"`
+	Estado int     `json:"state"`
+	Padre  int     `json:"pidPadre"`
+	Chil   []child `json:"child"`
 }
+
 type Cpu struct {
 	Porcentaje int       `json:"cpu_porcentaje"`
 	Procesos   []Process `json:"processes"`
@@ -56,116 +42,23 @@ type Ram struct {
 	Porcentaje int `json:"porcentaje"`
 }
 
-type Respuestacpu struct {
-	Mensaje string `json:"mensaje"`
-}
-
-type Respuestaram struct {
-	Mensaje string `json:"mensaje"`
-}
-
 var process *exec.Cmd
-var mongoClient *mongo.Client
-
-func StartProcess(w http.ResponseWriter, r *http.Request) {
-	// Crear un nuevo proceso con un comando de espera
-	cmd := exec.Command("sleep", "infinity")
-	err := cmd.Start()
-	if err != nil {
-		fmt.Print(err)
-		http.Error(w, "Error al iniciar el proceso", http.StatusInternalServerError)
-		return
-	}
-	// Obtener el comando con PID
-	process = cmd
-	fmt.Fprintf(w, "Proceso iniciado con PID: %d y estado en espera", process.Process.Pid)
-}
-
-func KillProcess(w http.ResponseWriter, r *http.Request) {
-	pidStr := r.URL.Query().Get("pid")
-	if pidStr == "" {
-		http.Error(w, "Se requiere el parámetro 'pid'", http.StatusBadRequest)
-		return
-	}
-	pid, err := strconv.Atoi(pidStr)
-	if err != nil {
-		http.Error(w, "El parámetro 'pid' debe ser un número entero", http.StatusBadRequest)
-		return
-	}
-	// Enviar señal SIGCONT al proceso con el PID proporcionado
-	cmd := exec.Command("kill", "-9", strconv.Itoa(pid))
-	err = cmd.Run()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error al intentar terminar el proceso con PID %d", pid), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "Proceso con PID %d ha terminado", pid)
-}
-
-func CPUModuleHandler(w http.ResponseWriter, r *http.Request) {
-	cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_1s2024")
-	outCpu, err := cmdCpu.CombinedOutput()
-	if err != nil {
-		fmt.Println("error", err)
-	}
-	var cpu_info Cpu
-	err = json.Unmarshal([]byte(outCpu), &cpu_info)
-	if err != nil {
-		fmt.Println(err)
-	}
-	//Mandar respuesta
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode((cpu_info))
-}
-
-func RAMModuleHandler(w http.ResponseWriter, r *http.Request) {
-	cmdRam := exec.Command("sh", "-c", "cat /proc/ram_so1_1s2024")
-	outRam, err := cmdRam.CombinedOutput()
-	if err != nil {
-		fmt.Println("error", err)
-	}
-	var ram_info Ram
-	err = json.Unmarshal([]byte(outRam), &ram_info)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode((ram_info))
-
-}
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNoContent)
-	}).Methods("GET", "POST")
-	router.HandleFunc("/api/cpu", CPUModuleHandler).Methods("GET", "POST")
-	router.HandleFunc("/api/ram", RAMModuleHandler).Methods("GET", "POST")
 
-	router.HandleFunc("/api/start", StartProcess)
-	router.HandleFunc("/api/kill", KillProcess)
+	app := fiber.New()
 
-	// Configurar conexión a MongoDB
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
-	var err error
-	mongoClient, err = mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
+	if err := Database.Connect(); err != nil {
 		log.Fatal(err)
+		fmt.Println("Error en la base de datos")
 	}
 
-	err = mongoClient.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
+	if err := app.Listen(":3000"); err != nil {
+		fmt.Println("Error en el servidor")
 	}
 
-	fmt.Println("Conexión a MongoDB establecida")
-
-	go func() {
-		log.Fatal(http.ListenAndServe(":5200", handlers.CORS()(router)))
-	}()
 	interval := 5
+	fmt.Println("Intervalo de tiempo: ", interval)
 
 	ticker := time.NewTicker(time.Second * time.Duration(interval))
 	defer ticker.Stop()
@@ -176,20 +69,10 @@ func main() {
 			now := time.Now()
 			second1 := now.Second()
 			re := strconv.Itoa(now.Year()) + "-" + fmt.Sprintf("%02d", now.Month()) + "-" + fmt.Sprintf("%02d", now.Day()) + "-" + fmt.Sprintf("%02d", now.Hour()) + "-" + fmt.Sprintf("%02d", now.Minute()) + "-" + fmt.Sprintf("%02d", second1)
-			fmt.Println("re")
+			// fmt.Println("re")
 			fmt.Println(re)
 
-			// Check if the CPU module file exists
-			if _, err := os.Stat("/proc/cpu_so1_jun2024"); os.IsNotExist(err) {
-				// If the file does not exist, load the module
-				cmd := exec.Command("sudo", "insmod", "/home/taro/Modulos/CPU/cpu.ko")
-				if err := cmd.Run(); err != nil {
-					fmt.Println("error loading module:", err)
-					return
-				}
-			}
-
-			cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_jun2024")
+			cmdCpu := exec.Command("sh", "-c", "cat /proc/cpu_so1_1s2024")
 			outCpu, err := cmdCpu.CombinedOutput()
 			if err != nil {
 				fmt.Println("error", err)
@@ -197,6 +80,16 @@ func main() {
 			}
 
 			//---CPU
+
+			cpuFree := exec.Command("mpstat", "1", "1")
+			var out bytes.Buffer
+			cpuFree.Stdout = &out
+			err = cpuFree.Run()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
 			fmt.Println("-------------------- CPU --------------------")
 			var cpuInfo Cpu
 			err = json.Unmarshal(outCpu, &cpuInfo)
@@ -205,32 +98,46 @@ func main() {
 				return
 			}
 
-			fmt.Println("porcentaje:", cpuInfo.Porcentaje)
+			output := out.String()
+			// Dividir la salida en líneas
+			lines := strings.Split(output, "\n")
+
+			// Buscar la línea que contiene los datos agregados (all)
+			var idleStr string
+			for _, line := range lines {
+				if strings.Contains(line, "all") {
+					// Dividir la línea en campos por espacios y extraer el valor de %idle
+					fields := strings.Fields(line)
+					if len(fields) >= 11 {
+						idleStr = fields[10]
+					}
+					break
+				}
+			}
+
+			// Convertir el valor de %idle a float
+			idle, err := strconv.ParseFloat(idleStr, 64)
+			if err != nil {
+				fmt.Println("Error parseando el valor de %idle:", err)
+				return
+			}
+
+			// Calcular el porcentaje libre de CPU
+			freeCPU := idle
+			fmt.Printf("Porcentaje libre de CPU: %.2f%%\n", freeCPU)
 			fmt.Println("cpu informacion de processos: ", cpuInfo.Procesos)
-			//controller.insertdata(Datacpu{"CPU%":  Porcentaje: cpuInfo.Porcentaje})
 
 			//Mandar respuesta (ejemplo simple de uso)
 			for _, proceso := range cpuInfo.Procesos {
-				// fmt.Printf("Process ID: %d, Name: %s, User: %d, State: %d, RAM: %d\n",
-				// 	proceso.Pid, proceso.Nombre, proceso.Usuario, proceso.Estado, proceso.Ram)
 				for _, child := range proceso.Chil {
 					fmt.Printf("  Child Process ID: %d, Name: %s, State: %d, Parent ID: %d\n",
 						child.Pid, child.Nombre, child.Estado, child.Padre)
 				}
 			}
-			//go sendToAPI("/cpu", cpuInfo.Porcentaje)
+
 			fmt.Println(" ==================== DATOS MODULO RAM ==================== ")
 			fmt.Println(" ")
 
-			// Check if the CPU module file exists
-			if _, err := os.Stat("/proc/ram_so1_jun2024"); os.IsNotExist(err) {
-				// If the file does not exist, load the module
-				cmd := exec.Command("sudo", "insmod", "/home/taro/Modulos/RAM/ram.ko")
-				if err := cmd.Run(); err != nil {
-					fmt.Println("error loading module:", err)
-					return
-				}
-			}
 			cmdRam := exec.Command("sh", "-c", "cat /proc/ram_so1_jun2024")
 			outRam, err := cmdRam.CombinedOutput()
 			if err != nil {
@@ -243,76 +150,10 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			go sendToAPI("/ram", ram_info.Porcentaje)
-			go sendToAPI("/cpu", cpuInfo.Porcentaje)
-			// fmt.Println(ram_info.Porcentaje)
-			// fmt.Println(ram_info.En_uso)
 
+			fmt.Println("Porcentaje de RAM:", ram_info.Porcentaje)
+			fmt.Println("RAM en uso:", ram_info.En_uso)
+			fmt.Println("RAM libre:", ram_info.Libre)
 		}
 	}
 }
-
-func sendToAPI(endpoint string, data interface{}) {
-	url := "http://34.171.101.34:5200" + endpoint
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Enviar datos a la API
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Insertar datos en MongoDB
-	collectionName := ""
-	if endpoint == "/cpu" {
-		collectionName = "cpu_data"
-	} else if endpoint == "/ram" {
-		collectionName = "ram_data"
-	}
-
-	collection := mongoClient.Database("mydatabase").Collection(collectionName)
-	_, err = collection.InsertOne(context.TODO(), data)
-	if err != nil {
-		fmt.Println("Error inserting data into MongoDB:", err)
-	}
-}
-
-// func sendToAPI(route string, data interface{}) {
-
-// 	url := fmt.Sprintf("http://localhost:5200/api%s", route)
-
-// 	jsonData, err := json.Marshal(data)
-// 	if err != nil {
-// 		log.Println("Error al convertir datos a JSON:", err)
-// 		return
-// 	}
-
-// 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-// 	if err != nil {
-// 		log.Println("Error al enviar datos a la API:", err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		log.Println("La API respondió con un código de estado no válido:", resp.StatusCode)
-// 		return
-// 	}
-
-// 	log.Printf("Datos enviados a la ruta %s\n", route)
-
-// }
