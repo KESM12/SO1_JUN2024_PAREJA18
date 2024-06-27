cd44f5377d9165144757912079f317bc6d2388d4
Kevin Secaida <3001920290101@ingenieria.usac.edu.gt>
2024-06-27 00:47:38 -0600

    Actualización
    Archivos varios alv

--- Proyecto2/gRPC/clientGRPC/main.go
+++ Proyecto2/gRPC/clientGRPC/main.go
@@ -19,17 +19,24 @@ type Data struct {
 }
 
 func sendData(c *fiber.Ctx) error {
-	var data []Data // Change to slice of Data
+	var data map[string]string
 	e := c.BodyParser(&data)
 	if e != nil {
 		return e
 	}
 
-	// Initialize gRPC connection
-	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	tweet := Data{
+		Texto: data["texto"],
+		Pais:  data["pais"],
+	}
+
+	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+
+	cl := pb.NewGetInfoClient(conn)
 	defer func(conn *grpc.ClientConn) {
 		err := conn.Close()
 		if err != nil {
@@ -37,84 +44,26 @@ func sendData(c *fiber.Ctx) error {
 		}
 	}(conn)
 
-	// Create a client for the gRPC service
-	cl := pb.NewGetInfoClient(conn)
-
-	// Iterate over each object in the JSON array and send it to the gRPC server
-	for _, tweet := range data {
-		ret, err := cl.ReturnInfo(context.Background(), &pb.RequestId{
-			Texto: tweet.Texto,
-			Pais:  tweet.Pais,
-		})
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("Respuesta del servidor ", ret)
+	ret, err := cl.ReturnInfo(ctx, &pb.RequestId{
+		Texto: tweet.Texto,
+		Pais:  tweet.Pais,
+	})
+	if err != nil {
+		return err
 	}
 
-	return nil
+	fmt.Println("Respuesta del servidor ", ret)
+
+	return c.JSON("Tweet enviado.")
 }
 
 func main() {
 	app := fiber.New()
 
-	// Endpoint to handle POST requests
 	app.Post("/insert", sendData)
 
-	// Start the Fiber server
 	err := app.Listen(":3000")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
-// func sendData(c *fiber.Ctx) error {
-// 	var data map[string]string
-// 	e := c.BodyParser(&data)
-// 	if e != nil {
-// 		return e
-// 	}
-
-// 	tweet := Data{
-// 		Texto: data["texto"],
-// 		Pais:  data["pais"],
-// 	}
-
-// 	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
-// 		grpc.WithBlock())
-// 	if err != nil {
-// 		log.Fatalf("did not connect: %v", err)
-// 	}
-
-// 	cl := pb.NewGetInfoClient(conn)
-// 	defer func(conn *grpc.ClientConn) {
-// 		err := conn.Close()
-// 		if err != nil {
-// 			log.Fatalf("could not close connection: %v", err)
-// 		}
-// 	}(conn)
-
-// 	ret, err := cl.ReturnInfo(ctx, &pb.RequestId{
-// 		Texto: tweet.Texto,
-// 		Pais:  tweet.Pais,
-// 	})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	fmt.Println("Respuesta del servidor ", ret)
-
-// 	return nil
-// }
-
-// func main() {
-// 	app := fiber.New()
-
-// 	app.Post("/insert", sendData)
-
-// 	err := app.Listen(":3000")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
