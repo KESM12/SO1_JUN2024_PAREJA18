d80f23cf2a8610bf87e07df2f72925a61ab92297
Kevin Secaida <3001920290101@ingenieria.usac.edu.gt>
2024-06-27 23:54:05 -0600

    Actualización
    Actualización archivos para redis grafana, kepler.

--- Proyecto2/gRPC/clientGRPC/main.go
+++ Proyecto2/gRPC/clientGRPC/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	pb "clientGRPC/client"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"google.golang.org/grpc"
@@ -18,7 +22,30 @@ type Data struct {
 	Pais  string
 }
 
+func sendToRust(data *Data) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:8000/set", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(res.Body)
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+}
+
 func sendData(c *fiber.Ctx) error {
+	/* API REST */
 	var data map[string]string
 	e := c.BodyParser(&data)
 	if e != nil {
@@ -30,6 +57,14 @@ func sendData(c *fiber.Ctx) error {
 		Pais:  data["pais"],
 	}
 
+	go sendGrpcServer(tweet)
+	go sendToRust(&tweet)
+
+	return nil
+}
+
+func sendGrpcServer(tweet Data) {
+	/* GRPC Client */
 	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock())
 	if err != nil {
@@ -49,12 +84,10 @@ func sendData(c *fiber.Ctx) error {
 		Pais:  tweet.Pais,
 	})
 	if err != nil {
-		return err
+		log.Fatal(err)
+	} else {
+		fmt.Println("Respuesta del servidor ", ret)
 	}
-
-	fmt.Println("Respuesta del servidor ", ret)
-
-	return c.JSON("Tweet enviado.")
 }
 
 func main() {
@@ -67,3 +100,42 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// func sendData(c *fiber.Ctx) error {
+// 	var data map[string]string
+// 	e := c.BodyParser(&data)
+// 	if e != nil {
+// 		return e
+// 	}
+
+// 	tweet := Data{
+// 		Texto: data["texto"],
+// 		Pais:  data["pais"],
+// 	}
+
+// 	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
+// 		grpc.WithBlock())
+// 	if err != nil {
+// 		log.Fatalf("did not connect: %v", err)
+// 	}
+
+// 	cl := pb.NewGetInfoClient(conn)
+// 	defer func(conn *grpc.ClientConn) {
+// 		err := conn.Close()
+// 		if err != nil {
+// 			log.Fatalf("could not close connection: %v", err)
+// 		}
+// 	}(conn)
+
+// 	ret, err := cl.ReturnInfo(ctx, &pb.RequestId{
+// 		Texto: tweet.Texto,
+// 		Pais:  tweet.Pais,
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	fmt.Println("Respuesta del servidor ", ret)
+
+// 	return c.JSON("Tweet enviado.")
+// }
--- Proyecto2/redis-rust/src/main.rs
+++ Proyecto2/redis-rust/src/main.rs
@@ -6,14 +6,14 @@ use redis::Commands;
 
 #[derive(Deserialize, Serialize)]
 struct Data {
-    texto: String,
-    pais: String,
+    Texto: String,
+    Pais: String,
 }
 
 #[post("/set", format = "json", data = "<data>")]
 async fn set_data(data: Json<Data>) -> Result<&'static str, &'static str> {
     // Crear cliente de redis
-    let client = redis::Client::open("redis://redis:6379/")
+    let client = redis::Client::open("redis://redis:6379/") //localhost
         .map_err(|_| "Error al crear el cliente de Redis")?;
 
     // Conexion a redis
@@ -21,7 +21,7 @@ async fn set_data(data: Json<Data>) -> Result<&'static str, &'static str> {
         .map_err(|_| "Error al conectarse con redis.")?;
 
     //Insertar hash en redis 
-    let _: () = con.hincr("paises", &data.pais, 1)
+    let _: () = con.hincr("paises", &data.Pais, 1)
     .map_err(|_| "Failed to set data in Redis")?;
     Ok("Data set")
 }
